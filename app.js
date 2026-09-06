0b180d4aa210d3eeab1e30fe422c5cb00cf4c99c
arinfracon <infraconar@gmail.com>
2026-09-07 00:48:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -391,7 +391,7 @@
                 title: "Kitchen",
                 images: [
                    "images/kitchen1.jpg",
-                   "images/1.jpg",
+                   "1.jpg",
                    "images/2.jpg",
                    "images/3.jpg",
                    "images/4.jpg",
