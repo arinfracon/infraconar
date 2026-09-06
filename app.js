a8cc4b1d64f14c08ac7782e5e4a379f13b6114d1
arinfracon <infraconar@gmail.com>
2026-09-07 00:25:57 +0530

    app.js

--- app.js
+++ app.js
@@ -390,9 +390,9 @@
             "kitchen-designs": {
                 title: "Kitchen",
                 images: [
-                    "images/kitchen1.jpg"
-                    "images/1.jpg",
-                    "images/2.jpg",
+                   "images/kitchen1.jpg"
+                   "images/1.jpg",
+                   "images/2.jpg",
                    "images/3.jpg",
                    "images/4.jpg",
                    "images/5.jpg",
